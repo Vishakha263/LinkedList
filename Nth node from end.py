fd6c7d73b0c10894694e770ea82da8ff47128b36
Vishakha Nerkar <47683468+Vishakha263@users.noreply.github.com>
2021-08-11 20:52:07 +0530

    Create Nth node from end.py
    PS: https://practice.geeksforgeeks.org/problems/nth-node-from-end-of-linked-list/1/?difficulty[]=0&page=2&category[]=Linked%20List&query=difficulty[]0page2category[]Linked%20List#

---
+++ Nth node from end.py
@@ -0,0 +1,86 @@
+#User function Template for python3
+'''
+	Your task is to return the data stored in
+	the nth node from end of linked list.
+	
+	Function Arguments: head (reference to head of the list), n (pos of node from end)
+	Return Type: Integer or -1 if no such node exits.
+
+	{
+		# Node Class
+		class Node:
+		    def __init__(self, data):   # data -> value stored in node
+		        self.data = data
+		        self.next = None
+	}
+'''
+#Function to find the data of nth node from the end of a linked list
+def getNthFromLast(head,n):
+    #code here
+    
+    ptr1=head
+    ptr2=head
+    
+    for i in range (0,n-1):
+        if ptr2.next is not None:
+            ptr2=ptr2.next
+        else:
+            return -1
+        
+    while(ptr2.next is not None):
+        ptr1=ptr1.next
+        ptr2=ptr2.next
+        
+    return ptr1.data
+
+#{ 
+#  Driver Code Starts
+#Initial Template for Python 3
+#Contributed by : Nagendra Jha
+
+import atexit
+import io
+import sys
+
+_INPUT_LINES = sys.stdin.read().splitlines()
+input = iter(_INPUT_LINES).__next__
+_OUTPUT_BUFFER = io.StringIO()
+sys.stdout = _OUTPUT_BUFFER
+
+@atexit.register
+
+def write():
+    sys.__stdout__.write(_OUTPUT_BUFFER.getvalue())
+    
+# Node Class
+class Node:
+    def __init__(self, data):   # data -> value stored in node
+        self.data = data
+        self.next = None
+
+# Linked List Class
+class LinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    # creates a new node with given value and appends it at the end of the linked list
+    def append(self, new_value):
+        new_node = Node(new_value)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+            return
+        self.tail.next = new_node
+        self.tail = new_node
+
+if __name__ == '__main__':
+    t=int(input())
+    for cases in range(t):
+        n,nth_node = map(int, input().strip().split())
+        a = LinkedList() # create a new linked list 'a'.
+        nodes_a = list(map(int, input().strip().split()))
+        for x in nodes_a:
+            a.append(x)  # add to the end of the list
+        print(getNthFromLast(a.head,nth_node))
+# } Driver Code Ends
